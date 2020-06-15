7d06d7b929bed22d2c16e59ce67beedf651d6c1b
Jim Saari <jim.saari97@gmail.com>
2020-06-15 20:38:23 +0300

    replace rebass with chakra-ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,10 @@
 import * as React from "react";
 import { MainLayout } from "components/layout/main";
-import { Canvas } from "components/canvas";
 
 export const App: React.FC = () => {
   return (
     <MainLayout>
-      <Canvas />
+      <div />
     </MainLayout>
   );
 };
